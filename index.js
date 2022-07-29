1a9a37d87e4bbbbb1b3eb16dd35f6565d1b66af1
Ishan <ishanwijes@gmail.com>
2022-07-29 18:07:38 +1000

    finished functionality of while loop that continually generates prompts until the user wishes to stop

--- index.js
+++ index.js
@@ -11,30 +11,131 @@ const { writeFile } = require('fs');
 
 let condition;
 let answerObject;
+let managerArray;
+let engineerArray;
+let internArray;
+
+let allRolesArray = [];
+let engineerP;
+let internP;
+
+
+
+
+// // PUT ASYNC AWAIT INTO TRY-CATCH BLOCKS!!!
+
+//    // add another inquirer.prompt in here to collect data about engineer
+//    const asyncEngineerFunction = async function() {
+
+//     const p = await inquirer.prompt([
+//         {
+//             type: 'input',
+//             message: "Enter the Engineer's name, employee ID, email address and GitHub username. Give your answers seperated by a comma. EG - James Martin, 20119, jamesmartin@gmail.com, james99martin",
+//             name: 'engineerDetails' 
+//         },
+//     ]).then(engineerDetailsObject => {
+
+//         engineerArray = engineerDetailsObject.engineerDetails.split(', ')
+
+//         allRolesArray.push(engineerArray)
+        
+//     })
+
+// }
+
+
+// const asyncInternFunction = async function() {
+
+//     const p = await inquirer.prompt([
+//         {
+//             type: 'input',
+//             message: "Enter the Intern's name, employee ID, email address and school. Give your answers seperated by a comma. EG - Dylan Mark, 19897, dylanmark@gmail.com, dylan101",
+//             name: 'internDetails' 
+//         },
+//     ]).then(internDetailsObject => {
+
+//         internArray = internDetailsObject.internDetails.split(', ')
+
+//         allRolesArray.push(internArray)
+        
+//     })   
+
+// }
+
 
 
 
 const asyncPrompter = async function() {
 
+    // the first question is always about the manager
+    await inquirer.prompt([
+        {
+            type: 'input',
+            message: "Enter the team manager's name, employee ID, email address and office number. Give your answers seperated by a comma. EG - John Smith, 19983, johnsmith@gmail.com, 36",
+            name: 'managerDetails' 
+        },
+    ]).then(managerDetailsObject => {
+
+        // create array basef of managerDetailsObject
+        managerArray = managerDetailsObject.managerDetails.split(', ')
+        
+    })
+
+
+
+
+
     while(condition !== 'Finish') {
 
-        answerObject = await inquirer.prompt([
+        const p = await inquirer.prompt([
             {
                 type: 'list',
                 choices: ['Engineer', 'Intern', 'Finish'],
+                message: 'add an Engineer or an Intern or finish',
                 name: 'userChoice' 
             },
-        ]).then(answerObject => {
-            console.log(answerObject)
+        ])
 
-            if(answerObject.userChoice === 'Engineer') {
-                condition = 'Engineer'
-            } else if(answerObject.userChoice === 'Intern') {
-                condition = 'Intern'
-            } else if(answerObject.userChoice === 'Finish') {
-                condition = 'Finish'
-            }
-        })
+
+
+        if(p.userChoice === 'Engineer') {
+            condition = 'Engineer'
+
+            engineerP = await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: "Enter the Engineer's name, employee ID, email address and GitHub username. Give your answers seperated by a comma. EG - James Martin, 20119, jamesmartin@gmail.com, james99martin",
+                    name: 'engineerDetails' 
+                },
+            ])
+
+            engineerArray = engineerP.engineerDetails.split(', ')
+
+            allRolesArray.push(engineerArray)
+          
+            
+        } else if(p.userChoice === 'Intern') {
+            condition = 'Intern'
+
+            internP = await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: "Enter the Intern's name, employee ID, email address and school. Give your answers seperated by a comma. EG - Dylan Mark, 19897, dylanmark@gmail.com, dylan101",
+                    name: 'internDetails' 
+                },
+            ])
+            
+            internArray = internP.internDetails.split(', ')
+
+            allRolesArray.push(internArray)
+            
+        } else if(p.userChoice === 'Finish') {
+            condition = 'Finish'
+
+            console.log(allRolesArray);
+
+        }
+
 
     }
 
@@ -46,11 +147,8 @@ asyncPrompter()
 
 
 
-// while(condition !== 'Finish') {
 
-//     asyncPrompter()
 
-// }
 
 
 
